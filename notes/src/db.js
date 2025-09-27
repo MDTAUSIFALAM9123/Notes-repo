505013c1755916ca5bb19aa4389646817d141f97
mohd saif <msaifansari13@gmail.com>
2025-09-27 18:29:58 +0530

    nothing

--- notes/src/db.js
+++ notes/src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/notesDB";
+    const uri = process.env.MONGO_URI || "**";
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
